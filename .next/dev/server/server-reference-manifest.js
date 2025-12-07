82ffc4400d75d6f6a24317946dd1b3375f975ea7
chon1029 <35348507+chon1029@users.noreply.github.com>
2025-12-07 22:24:37 +0100

    Delete .next directory

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"2iAEBJxfenYpoF6OD5Q2ELj7gumyrKG3G9CNW1DHqVg=\"\n}"
--- .next/dev/static/chunks/src_app_page_tsx_a30fd3ff._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_e22b0631._.js",
-  "static/chunks/src_components_f9b79502._.js"
-],
-    source: "dynamic"
-});
